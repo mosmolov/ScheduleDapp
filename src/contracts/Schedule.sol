8f662e81e2f73a740b3d4a9eff1ac541708aa748
Michael Osmolovskiy <mosmolovskiy@gmail.com>
2022-03-14 09:41:43 -0400

    add hardhat config, smart contract

--- src/contracts/Schedule.sol
+++ src/contracts/Schedule.sol
@@ -1,20 +1,29 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.7.3;
 
-contract Schedule{
-    struct mySchedule{
+contract Schedule {
+    struct Event {
+        uint256 order;
+        uint256 startTime;
+        uint256 endTime;
         string name;
-        mapping(uint256 => string) schedule;
     }
-
+    struct mySchedule {
+        string date;
+        mapping(uint256 => Event) myEvents;
+    }
+    string public name;
+    string public date;
     mySchedule private schedule;
-    event UpdatedSchedule();
 
-    constructor(){
+    event updatedSchedule(string eventName, uint256 startTime, uint256 endTime);
 
+    constructor(string memory myName, string memory myDate) {
+        name = myName;
+        date = myDate;
     }
-    function addTimes(uint256 startTime, string endTime, string event,)
-    {
-
+    function addEvent(Event memory newEvent) private {
+        schedule.myEvents[(newEvent.order) + 1] = newEvent;
+        emit updatedSchedule(newEvent.name, newEvent.startTime, newEvent.endTime);
     }
 }
